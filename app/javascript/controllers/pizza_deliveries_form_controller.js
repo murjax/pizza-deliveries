bdc902975b2cfdfb793173136d5586ed62b59fa6
Ryan Murphy <murjax@gmail.com>
2024-03-23 12:23:18 -0400

    Part 5: Add location search

--- app/javascript/controllers/pizza_deliveries_form_controller.js
+++ app/javascript/controllers/pizza_deliveries_form_controller.js
@@ -4,7 +4,8 @@ export default class extends Controller {
   static targets = [
     "map",
     "latitude",
-    "longitude"
+    "longitude",
+    "searchField"
   ];
 
   mapTargetConnected() {
@@ -31,11 +32,17 @@ export default class extends Controller {
       center: new google.maps.LatLng(this.data.get("latitude") || 39.5, this.data.get("longitude") || -98.35),
       zoom: 4,
       mapId: "map-id",
-    })
+    });
+
+    this.autocomplete = new google.maps.places.Autocomplete(this.searchFieldTarget)
+    this.autocomplete.bindTo("bounds", this.map)
+    this.autocomplete.setFields(["address_components", "geometry", "icon", "name"])
+    this.autocomplete.addListener("place_changed", this.placeChanged.bind(this))
 
     this.marker = new AdvancedMarkerElement({
       map: this.map
-    })
+    });
+
     if (this.data.get("latitude") && this.data.get("longitude")) {
       this.marker.position = {
         lat: Number(this.data.get("latitude")),
@@ -53,5 +60,31 @@ export default class extends Controller {
       context.latitudeTarget.value = event.latLng.lat();
       context.longitudeTarget.value = event.latLng.lng();
     })
+  }
+
+   placeChanged() {
+    let place = this.autocomplete.getPlace();
+
+    if (!place.geometry) {
+      return;
+    }
+
+    if (place.geometry.viewport) {
+      this.map.fitBounds(place.geometry.viewport);
+    } else {
+      this.map.setCenter(place.geometry.location);
+      this.map.setZoom(this.data.get("latitude") == null ? 4 : 17);
+    }
+
+    this.marker.position = place.geometry.location;
+
+    this.latitudeTarget.value = place.geometry.location.lat();
+    this.longitudeTarget.value = place.geometry.location.lng();
+  }
+
+  keydown(event) {
+    if (event.key == "Enter") {
+      event.preventDefault();
+    }
   }
 }
