ebc3f96442dc1c8c8209fc2c7463fed010a3f6a0
Ryan Murphy <murjax@gmail.com>
2024-03-23 12:06:42 -0400

    Part 2: Add markers

--- app/javascript/controllers/pizza_deliveries_index_map_controller.js
+++ app/javascript/controllers/pizza_deliveries_index_map_controller.js
@@ -21,11 +21,25 @@ export default class extends Controller {
 
   async initIndexMap() {
     const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    const url = new URL(window.location.href);
 
     this.map = new Map(this.mapTarget, {
       center: new google.maps.LatLng(39.5, -98.35),
       zoom: 4,
       mapId: "map-id"
     });
+
+    fetch(`/pizza_deliveries.json${url.search}`)
+      .then(response => response.json())
+      .then(data => {
+        data.data.forEach(pizzaDelivery => {
+          let marker = new AdvancedMarkerElement({
+            map: this.map,
+            position: { lat: parseFloat(pizzaDelivery.latitude), lng: parseFloat(pizzaDelivery.longitude) }
+          });
+        })
+      });
   }
 }
